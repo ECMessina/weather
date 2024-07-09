ac40d9c38a21dcbb460c93c877e92bc3dc7b3051
Liz <ecmessina@gmail.com>
2024-07-09 12:41:53 -0400

    Weather response from url

--- lib/current_location_weather.dart
+++ lib/current_location_weather.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather/constants.dart';
+import 'package:weather/weather_response.dart';
+import 'package:weather/weather_service.dart';
 
-class CurrentLocationWeather extends StatelessWidget {
+class CurrentLocationWeather extends StatefulWidget {
   const CurrentLocationWeather({super.key, required this.position});
 
   final Position position;
 
+  @override
+  State<CurrentLocationWeather> createState() => _CurrentLocationWeatherState();
+}
+
+class _CurrentLocationWeatherState extends State<CurrentLocationWeather> {
+  WeatherResponse? weatherResponse;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getWeather();
+  }
+
+  void getWeather() async {
+    weatherResponse = await WeatherService.getWeatherByLatLong(widget.position);
+    setState(() {});
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ lib/weather_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather_response.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+  static const _baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  static const _apiKey = '84ebe15e64b9de33720d14b09af5b38c';
+
+  static Future<WeatherResponse> getWeatherByLatLong(Position position) async {
+    final response = await http.get(Uri.parse('$_baseUrl?lat=${position.latitude}&lon=${position.longitude}&appid=$_apiKey&units=imperial'));
+    // await Future.delayed(const Duration(seconds: 2));
+    if (response.statusCode == 200) {
+      return WeatherResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+
+  static Future<WeatherResponse> getWeatherByName(String name) async {
+    final response = await http.get(Uri.parse('$_baseUrl?q=$name&appid=$_apiKey&units=imperial'));
+
+    if (response.statusCode == 200) {
+      return WeatherResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+}
